547e480d2d2ea5b87413f4bed28331b25dc9e8da
abdurrehman-135 <ar.ansari.124@westcliff.edu>
2026-03-23 09:46:29 +0300

    Create the createList component and functionalities

--- crud-json-server/src/App.js
+++ crud-json-server/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Lists from "./Lists";
+import CreateList from "./CreateList";
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -17,21 +18,70 @@ class App extends React.Component {
     fetch("http://localhost:5000/posts")
       .then((res) => res.json())
       .then((result) => {
-        this.setState({ 
-          loading: false, 
-          alldata: result });
+        this.setState({
+          loading: false,
+          alldata: result,
+        });
       })
       .catch((error) => console.error("Error fetching data:", error));
   };
+
+  handleChange = (event) => {
+    let title = this.state.singledata.title;
+    let author = this.state.singledata.author;
+
+    if (event.target.name === "title") {
+      title = event.target.value;
+    } else {
+      author = event.target.value;
+    }
+
+    this.setState({
+      singledata: {
+        title: title,
+        author: author,
+      },
+    });
+  };
+
+  createList = () => {
+    fetch("http://localhost:5000/posts", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(this.state.singledata),
+    }).then(() => {
+      this.setState({
+        singledata: {
+          title: "",
+          author: "",
+        },
+      });
+    });
+  };
+
   render() {
-    const lisTable = this.state.loading ? ( <span>Loading Data... Please be patient.</span> ) : 
-    ( <Lists alldata={this.state.alldata} /> ); 
+    const lisTable = this.state.loading ? (
+      <span>Loading Data... Please be patient.</span>
+    ) : (
+      <Lists alldata={this.state.alldata} />
+    );
     return (
       <div className="container">
         <span className="title-bar">
-          <button type="button" className="btn btn-primary" onClick={this.getLists}>
+          <button
+            type="button"
+            className="btn btn-primary"
+            onClick={this.getLists}
+          >
             Get Lists
           </button>
+          <CreateList
+            singledata={this.state.singledata}
+            handleChange={this.handleChange}
+            createList={this.createList}
+          />
         </span>
         {lisTable}
       </div>
